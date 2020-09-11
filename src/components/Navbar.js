8ad539ba69905e4ec36610bb1adf5aa547043e05
Will Farley <a.will.farley@gmail.com>
2020-09-10 17:39:18 -0700

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
+import github from '../img/az-medicare-helpers-logo.png'
 import logo from '../img/logo.svg'
 
 const Navbar = class extends React.Component {
