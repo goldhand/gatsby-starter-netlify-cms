6c9e3a028c0f7168125d275234c24a6f5467386f
Will Farley <a.will.farley@gmail.com>
2020-09-11 00:54:59 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'AZ Medicare Helpers',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Providing Simple Answers to Your Medicare Questions',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
